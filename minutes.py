3c7ea0eff86112966cd4c7fc5dd3497124618014
Robert Cicero <robert.cicero@iress.com>
2020-01-16 08:17:58 +0000

    Setting up base repo

--- minutes.py
+++ minutes.py
@@ -1,12 +1,14 @@
 # NoteIt-Minutes-API
 
-# - Add Minutes
+# - Add Minute
 # Will need to tag the person creating it by email address
 # Will need to add the datetime of the meeting
 # Will need to add the participants
-# - Add Participants
 # - Tag Participants - will return an the user name if exists, other will just tag their email
-# - Get Minutes
-# - Archive Minutes
-# - Get Minute History
-#
+# - Get a specific minute
+# - Archive/Close Minute
+# - Get My Minute History
+# - Get Minutess I've been tagged in
+# - Create Minute Action
+# - Complete Minute Action
+# - Cancel Minute Action
