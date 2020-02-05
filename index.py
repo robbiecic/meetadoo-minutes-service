99b62adbbfa7028702901946b0749255b873507a
Robert Cicero <robert.cicero.rc@gmail.com>
2020-02-05 21:40:51 +0000

    Worked out a way to return json, i think

--- index.py
+++ index.py
@@ -61,9 +61,10 @@ def lambda_handler(event, context):
             # Get email from decoded response. Don't want to store it on client side, but it's in the token which is issued upon successful login
             body_email = authenticated_response['response']
             result = json.loads(get_my_minutes(body_email))
-            return json.dumps({"statusCode": result['statusCode'],
-                               "body": result['response']
-                               })
+            print("result" + str(result))
+            return {"statusCode": result['statusCode'],
+                    "body": result['response']
+                    }
         else:
             return authenticated_response
     else:
