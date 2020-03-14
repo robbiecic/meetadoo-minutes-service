fae9211b108651589d4441e657f0d01f93440e2d
Robert Cicero <robert.cicero.rc@gmail.com>
2020-03-14 09:57:06 +0000

    Updated AWS secret

--- aws.py
+++ aws.py
@@ -2,10 +2,10 @@ import boto3
 
 
 def create_dynamodb_client():
-    return boto3.client('dynamodb', region_name='ap-southeast-2', aws_access_key_id="AKIAJVJV2E22WXY332VA",
-                        aws_secret_access_key="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")
+    return boto3.client('dynamodb', region_name='ap-southeast-2', aws_access_key_id="AKIAI2BPLIHQVXLWSGCQ",
+                        aws_secret_access_key="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")
 
 
 def create_dynamodb_resource():
-    return boto3.resource('dynamodb', region_name='ap-southeast-2', aws_access_key_id="AKIAJVJV2E22WXY332VA",
-                          aws_secret_access_key="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")
+    return boto3.resource('dynamodb', region_name='ap-southeast-2', aws_access_key_id="AKIAI2BPLIHQVXLWSGCQ",
+                          aws_secret_access_key="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")
